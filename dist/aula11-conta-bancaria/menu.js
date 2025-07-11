2a28f0e34e5826b0239962dbcd9e6ab5ab782162
Larissa <larissaemini@gmail.com>
2025-07-10 21:49:03 -0300

    fix: .gitignore

---
+++ dist/aula11-conta-bancaria/menu.js
@@ -0,0 +1,32 @@
+import * as readlineSync from 'readline-sync';
+import { Colors } from "./util";
+export class Menu {
+    static iniciar() {
+        console.clear();
+        console.log(Colors.fgBlue + "Bem-vinda ao Banco Larissa!" + Colors.reset);
+        let opcao;
+        do {
+            console.log("\n1 - Ver Saldo");
+            console.log("2 - Depositar");
+            console.log("3 - Sacar");
+            console.log("0 - Sair");
+            opcao = parseInt(readlineSync.question("Escolha uma opção: "));
+            switch (opcao) {
+                case 1:
+                    console.log("Saldo: R$ 0");
+                    break;
+                case 2:
+                    console.log("Depósito realizado (exemplo)");
+                    break;
+                case 3:
+                    console.log("Saque realizado (exemplo)");
+                    break;
+                case 0:
+                    console.log("Saindo...");
+                    break;
+                default:
+                    console.log("Opção inválida!");
+            }
+        } while (opcao !== 0);
+    }
+}
---
+++ dist/aula12-conta-bancaria/src/menu.js
@@ -0,0 +1,52 @@
+import { Conta } from "./model/conta";
+import * as readlineSync from 'readline-sync';
+function main() {
+    // Criando uma lista de contas
+    const conta1 = new Conta(1, 'Larissa Ruiz', 1000);
+    const conta2 = new Conta(2, 'João Silva', 500);
+    const contas = [conta1, conta2];
+    while (true) {
+        console.log('\n=== MENU CONTA BANCÁRIA ===');
+        console.log('1 - Visualizar Contas');
+        console.log('2 - Depositar');
+        console.log('3 - Sacar');
+        console.log('0 - Sair');
+        const opcao = readlineSync.questionInt('Escolha uma opção: ');
+        switch (opcao) {
+            case 1:
+                console.log('\n--- Visualização das Contas ---');
+                contas.forEach((c) => c.imprimir());
+                break;
+            case 2:
+                console.log('\n--- Depósito ---');
+                const numeroDep = readlineSync.questionInt('Número da conta: ');
+                const valorDep = readlineSync.questionFloat('Valor para depósito: ');
+                const contaDep = contas.find((c) => c['numero'] === numeroDep);
+                if (contaDep) {
+                    contaDep.depositar(valorDep);
+                }
+                else {
+                    console.log('❌ Conta não encontrada.');
+                }
+                break;
+            case 3:
+                console.log('\n--- Saque ---');
+                const numeroSaq = readlineSync.questionInt('Número da conta: ');
+                const valorSaq = readlineSync.questionFloat('Valor para saque: ');
+                const contaSaq = contas.find((c) => c['numero'] === numeroSaq);
+                if (contaSaq) {
+                    contaSaq.sacar(valorSaq);
+                }
+                else {
+                    console.log('❌ Conta não encontrada.');
+                }
+                break;
+            case 0:
+                console.log('Saindo do programa...');
+                return;
+            default:
+                console.log('Opção inválida. Tente novamente.');
+        }
+    }
+}
+main();
